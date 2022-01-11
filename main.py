e8e67d7b06795b64626581526b9b3cf03db05f4c
Chris Dewey <chris.dewey310@gmail.com>
2022-01-11 08:37:34 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+# This image generator is created for generating images for the Ukiyoe Warriors NFT project.
+# There will be two sections: The image generator, in which multiple png files are placed together,
+#   along with some masking effects. Secondly, there will be a randomization function.
+
